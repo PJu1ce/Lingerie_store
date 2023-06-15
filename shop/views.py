4c9bd17258340bd44ce08df142dac70f25a93f8b
PJu1ce <81172828+PJu1ce@users.noreply.github.com>
2023-06-15 23:37:27 +0300

    Adding and redesigning templates

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.views import generic
+from django.http import HttpResponse
+
+class IndexView(generic.ListView):
+    template_name = 'shop/index.html'
+    context_object_name = 'shop'
+
+    def get_queryset(self):
+        return HttpResponse('')
 
 def product_list(request, category_slug=None):
     category = None
